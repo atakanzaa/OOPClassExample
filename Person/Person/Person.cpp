e78ad68df337d82d42e43de7cb3395330dc41aa7
atakanzaa <atakan.yel@hotmail.com>
2022-12-24 15:24:37 +0300

    Person class
    The project I've committed to see my own progress

---
+++ Person/Person/Person.cpp
@@ -0,0 +1,17 @@
+﻿#include "Person.h"
+#include <time.h>
+int main()
+{
+	srand(time(NULL));
+	Student studentArray[] = { {"Atakan","Yel",1000},{"Emre","Yel",1001},{"Aykan","Sayilkan",1002},{"Fatih","Ortatepe",1003} };
+	Teacher teacher1("Mustafa", "Yel", "Software");
+	for (int i = 0; i < 4; i++)
+	{
+		studentArray[i].setStudentGrade(rand() % 100 + 1);
+		studentArray[i].print();
+	}
+	teacher1.passFailStatus(studentArray);
+	teacher1.printStudentInformation(studentArray);
+	
+}
+
